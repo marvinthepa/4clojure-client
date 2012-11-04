51ee3d29259a6dd42299549f412c3ac01fa83d86
Maximilian Karasz <maximilian.karasz@googlemail.com>
2012-11-04 15:41:54 +0100

    - improved html2text rendering - force output width to 80 cols - view now with parameter that selects implicitly, reducing the   effective command set to view and propose - improved quoting for propose

--- src/fourclojure/repl.clj
+++ src/fourclojure/repl.clj
@@ -13,7 +13,16 @@
       (string/replace #"<.?code>" "")
       (string/replace #"<a.*?href=\"(.*?)\".*?>(.*?)<\/a>"
                       "$2 [$1]")
-      (string/replace #"<.?span.*?>" "")))
+      (string/replace #"<.?span.*?>" "")
+      (string/replace #"<.?br\s?.?>" "\n")
+      (string/replace #"\t\s" "")
+      (string/replace #"<.?ul>" "")
+      (string/replace #"<li>" "- ")
+      (string/replace #"<\/li>" "")
+      (string/replace #"<.?i>" "")
+      (string/replace #"&mdash;" "-")
+      (string/replace #"<.?strong>" "")
+      (string/replace #"\n{2,}" "\n\n")))
 
 (defn keywordize
   ([source]
@@ -35,30 +44,47 @@
                           (:id *current-problem*)
                           code))))
 
+(defn force-cols [cols s]
+  (string/replace (apply str
+                         (interpose "\n"
+                                    (re-seq #".{0,79}[\s\n]" (str s "\n"))))
+                  #"\n{2}" "\n"))
+
+(defn select [id]
+  (def ^:dynamic *current-problem*
+       (parse-problem (client/get-problem id) id)))
+
+
 ;;
 ;; Commands
 ;;
 
-(defn view []
-  (if *current-problem*
-    (let [{:keys [id title description tests]} *current-problem*]
-      (println
-       (str
-        id ". "title "\n"
-        (clean description) "\n\n"
-        "Tests:" "\n"
-        (apply str (interpose "\n"
-                              (map clean tests))))))
-    (println "No problem selected - use select first.")))
+
+(defn view
+  ([]
+     (if *current-problem*
+       (let [{:keys [id title description tests
+                     user difficulty restricted]} *current-problem*]
+         (println
+          (force-cols 80
+                      (str
+                       id ". *** " title " ***\n"
+                       "author: " user "\n"
+                       "level: " difficulty  "\n\n"
+                       (clean description) "\n\n"
+                       "Tests:" "\n"
+                       (apply str (interpose "\n"
+                                             (map clean tests)))
+                       (when (seq restricted)
+                         (str "\n\n" "Restrictions: " (pr-str restricted)))))))
+       (println "No problem selected - try (view <id>).")))
+  ([id]
+     (do (select id)
+         (view))))
 
 
-(defn select [id]
-  (def ^:dynamic *current-problem*
-       (parse-problem (client/get-problem id) id))
-  (view))
-
-(defmacro propose [body]
-  `(let [rsp# (remote-check '~body)]
+(defmacro propose [& body]
+  `(let [rsp# (remote-check ''~@body)]
      (println
       (str
        (if (string/blank? (:message rsp#))
